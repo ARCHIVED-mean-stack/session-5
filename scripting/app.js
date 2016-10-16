88bede6d667bf20f5aa827d7e33563f3cf1f23eb
DannyBoyNYC <daniel.deverell@gmail.com>
2016-10-16 15:25:28 -0400

    initial commit

--- scripting/app.js
+++ scripting/app.js
@@ -1,10 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync')
 var express = require('express');
-var app = express();
 
-app.set('port', process.env.PORT || 3000 );
+var sassOptions = {
+	errLogToConsole: true,
+	outputStyle: 'expanded'
+};
+
+var sassSources = './app/public/sass/**/*.scss';
+var sassOutput = './app/public/css';
+var htmlSource = './app/public/**/*.html';
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+gulp.task('sass', function(){
+	return gulp.src(sassSources)
+	.pipe(sourcemaps.init())
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest(sassOutput))
+	.pipe(browserSync.stream())
+});
+
+function listening () {
+	browserSync({
+		proxy: 'localhost:' + port,
+		browser: "google chrome"
+	});
+	gulp.watch(sassSources, ['sass']);
+	gulp.watch(htmlSource).on('change', browserSync.reload);
+}
+
 
 app.use(express.static('./app/public'));
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port'));
-});
+app.listen(port, listening);
